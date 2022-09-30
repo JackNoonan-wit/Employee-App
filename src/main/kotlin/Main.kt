551a01fa4be981926906475b2acf651af1fe5cf8
Jack Noonan <jackpersonal2018@gmail.com>
2022-10-01 00:27:54 +0100

    Changed how the payslip recieved its numbers, equations taken out of the payslip and made into separate functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package ie.setu
 
+import kotlin.math.round
+
 val firstName = "Joe"
 val surname = "Soap"
 val gender = "M"
@@ -18,9 +20,18 @@ val cycleToWorkMonthlyDeduction = 54.33
 
 fun main(args: Array<String>) {
     println("Payslip Printer")
+    println(getFullName())
     payslip()
     println("\n")
     payslipRounded()
+    getFullName()
+
+    println("Monthly Salary: ${getMonthlySalary()}")
+    println("Monthly PAYE: ${getMonthlyPAYE()}")
+    println("Monthly PRSI: ${getMonthlyPRSI()}")
+    println("Monthly Total Deductions: ${getMonthlyDeductions()}")
+    println("Monthly Pay: ${getMonthlyPay()}")
+    println("Monthly Net Salary: ${getNetMonthlyPay()}")
 }
 
 fun payslip() {
@@ -51,6 +62,20 @@ fun payslip() {
 |_______________________________________________________________________|""")
 }
 
+
+
+fun getFullName() = when(gender){
+    "m", "M" ->"Mr. $firstName $surname"
+    "f", "F" ->"Ms. $firstName $surname"
+    else -> "$firstName $surname"
+}
+fun getMonthlySalary()=roundTwoNumbers(grossSalary/12)
+fun getMonthlyPRSI()=roundTwoNumbers(getMonthlySalary() * (prsiPercentage/100))
+fun getMonthlyPAYE()=roundTwoNumbers(getMonthlySalary() * (payePercentage/100))
+fun getMonthlyPay()=roundTwoNumbers(getMonthlySalary() + (annualBonus / 12))
+fun getMonthlyDeductions()=roundTwoNumbers((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
+fun getNetMonthlyPay()=roundTwoNumbers(roundTwoNumbers(getMonthlyPay()-getMonthlyDeductions()))
+
 fun payslipRounded(){
 
     val monthlySalary = (grossSalary/12)
@@ -64,25 +89,27 @@ fun payslipRounded(){
 _______________________________________________________________________
                           Monthly Payslip                              
 _______________________________________________________________________                                                                      
-                Employee Name:  ${firstName.uppercase()} ${surname.uppercase()}(${gender.uppercase()})
+                Employee Name:  ${getFullName()} (${gender.uppercase()})
                 Employee ID: $employeeID                                                                       
 _______________________________________________________________________                                                               
-                PAYMENT DETAILS - Gross Pay: ${"%.2f".format(grossPay)} 
+                PAYMENT DETAILS - Gross Pay: ${getMonthlyPay()} 
 _______________________________________________________________________
-                Salary: ${"%.2f".format(monthlySalary)} 
-                Bonus:  ${"%.2f".format((annualBonus / 12))} 
+                Salary: ${(getMonthlySalary())} 
+                Bonus:  ${roundTwoNumbers(annualBonus / 12)} 
 _______________________________________________________________________
-                DEDUCTIONS - Total Deductions ${"%.2f".format(totalDeductions)}
+                DEDUCTIONS - Total Deductions ${getMonthlyDeductions()}
                 
                 Cycle To Work: $cycleToWorkMonthlyDeduction
-                PRSI: ${"%.2f".format(monthlyPrsi)}
-                PAYE: ${"%.2f".format(monthlyPaye)}
+                PRSI: ${getMonthlyPRSI()}
+                PAYE: ${getMonthlyPAYE()}
 _______________________________________________________________________
-                NET PAY:${"%.2f".format((grossPay - totalDeductions))}
+                NET PAY:${getNetMonthlyPay()}
 _______________________________________________________________________
 """)
 }
 
+
+fun roundTwoNumbers(number: Double) = round(number * 100) / 100
 
 
 
