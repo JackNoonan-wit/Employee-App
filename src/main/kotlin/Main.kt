4142b7be93390bef81835cd698774988b47deb53
Jack Noonan <jackpersonal2018@gmail.com>
2022-09-30 20:02:37 +0100

    Edit the payslip to be in one raw string and tidy it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,25 +31,26 @@ fun payslip() {
     val monthlyPaye = monthlySalary * (payePercentage / 100)
     val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
 
-    println ("_________________________________________________________________________")
-    println ("|                          Monthly Payslip                              |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|   Employee Name:  ${firstName.uppercase()} ${surname.uppercase()}(${gender.uppercase()})                    Employee ID: $employeeID    |")
-    println ("|                                                                       |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Salary: $monthlySalary\t\t PAYE: $monthlyPaye\t\t\t|")
-    println ("|    Bonus:  ${annualBonus / 12}\t\t\t\t PRSI: $monthlyPrsi\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: $cycleToWorkMonthlyDeduction\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|       Gross:  $grossPay \t\tTotal Deductions: $totalDeductions        |")
-    println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t NET PAY:${grossPay - totalDeductions} \t\t\t\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
+    println ("""_________________________________________________________________________
+|                          Monthly Payslip                              |
+|_______________________________________________________________________|
+|                                                                       |
+|   Employee Name:  ${firstName.uppercase()} ${surname.uppercase()}(${gender.uppercase()})                    Employee ID: $employeeID    |
+|                                                                       |
+|_______________________________________________________________________|
+|                                                                       |
+|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|
+|_______________________________________________________________________|
+|    Salary: $monthlySalary\t\t PAYE: $monthlyPaye\t\t\t|
+|    Bonus:  ${annualBonus / 12}\t\t\t\t PRSI: $monthlyPrsi\t\t\t|
+|    \t\t\t\t\t\t\t\t Cycle To Work: $cycleToWorkMonthlyDeduction\t\t\t\t|
+|_______________________________________________________________________|
+|       Gross:  $grossPay \t\tTotal Deductions: $totalDeductions        |
+|_______________________________________________________________________|
+|   \t\t\t\t NET PAY:${grossPay - totalDeductions} \t\t\t\t\t\t\t|
+|_______________________________________________________________________|""")
 }
+
 fun payslipRounded(){
 
     val monthlySalary = (grossSalary/12)
@@ -58,24 +59,28 @@ fun payslipRounded(){
     val grossPay = (monthlySalary + (annualBonus/12))
     val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
 
-    println ("_________________________________________________________________________")
-    println ("|                          Monthly Payslip                              |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|   Employee Name:  ${firstName.uppercase()} ${surname.uppercase()}(${gender.uppercase()})                    Employee ID: $employeeID    |")
-    println ("|                                                                       |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Salary: ${"%.2f".format(monthlySalary)}\t\t\t\t PAYE: ${"%.2f".format(monthlyPaye)}\t\t\t\t\t\t|")
-    println ("|    Bonus:  ${"%.2f".format((annualBonus / 12))}\t\t\t\t\t PRSI: ${"%.2f".format(monthlyPrsi)}\t\t\t\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: $cycleToWorkMonthlyDeduction\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Gross:  ${"%.2f".format(grossPay)} \t\t\t\tTotal Deductions: ${"%.2f".format(totalDeductions)} \t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t\t\t NET PAY:${"%.2f".format((grossPay - totalDeductions))} \t\t\t\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
+    println (
+"""
+_______________________________________________________________________
+                          Monthly Payslip                              
+_______________________________________________________________________                                                                      
+                Employee Name:  ${firstName.uppercase()} ${surname.uppercase()}(${gender.uppercase()})
+                Employee ID: $employeeID                                                                       
+_______________________________________________________________________                                                               
+                PAYMENT DETAILS - Gross Pay: ${"%.2f".format(grossPay)} 
+_______________________________________________________________________
+                Salary: ${"%.2f".format(monthlySalary)} 
+                Bonus:  ${"%.2f".format((annualBonus / 12))} 
+_______________________________________________________________________
+                DEDUCTIONS - Total Deductions ${"%.2f".format(totalDeductions)}
+                
+                Cycle To Work: $cycleToWorkMonthlyDeduction
+                PRSI: ${"%.2f".format(monthlyPrsi)}
+                PAYE: ${"%.2f".format(monthlyPaye)}
+_______________________________________________________________________
+                NET PAY:${"%.2f".format((grossPay - totalDeductions))}
+_______________________________________________________________________
+""")
 }
 
 
